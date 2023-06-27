39d696f899e353a447f5036baf4a737f5c9b637b
GabrielGG250 <gabe.academic@gmail.com>
2023-06-27 16:27:01 -0300

    opacity no menu mobile

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,14 @@
 const menu = document.querySelector('#menu-mobile');
 const abrirMenu = document.querySelector('#icon-menu');
-const fecharMenu = document.querySelector('#fechar-menu')
+const fecharMenu = document.querySelector('#fechar-menu');
+const fundo = document.querySelector('#fundo');
 
 abrirMenu.addEventListener('click', function() {
     menu.classList.remove('hide');
+    fundo.classList.remove('hide');
 })
 
 fecharMenu.addEventListener('click', function() {
     menu.classList.add('hide');
+    fundo.classList.add('hide');
 })
